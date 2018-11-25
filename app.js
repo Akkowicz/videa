ff4a655587865a3984e8bf82ca9a307745e9ffc6
Mateusz Przybyłowicz <uamfhq@gmail.com>
2018-11-25 16:55:57 +0100

    Bulk of work

--- app.js
+++ app.js
@@ -1,6 +1,93 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 5000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/videa', {
+    useNewUrlParser: true 
+})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+
+// Index route
+app.get('/', (req, res) => {
+    const title = 'Videa';
+    res.render('index', {
+        title
+    });
+});
+
+// About route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+        res.render('ideas/index', {
+            ideas
+        });
+    })
+})
+
+// Add Idea Form route
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+})
+
+// Submit Idea Route
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if (!req.body.details) {
+        errors.push({text: 'Please add some details'});
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+        };
+        new Idea(newUser)
+        .save()
+        .then(idea => {
+            res.redirect('/ideas');
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
