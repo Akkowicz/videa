ca690d1b98aa96226385b703cdd3c703f79ff1c6
Mateusz Przybyłowicz <uamfhq@gmail.com>
2018-11-29 12:58:35 +0100

    Work on edit state

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const app = express();
 const port = 5000;
+
+// allow for method override
+app.use(methodOverride('_method'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -46,18 +50,30 @@ app.get('/about', (req, res) => {
 // Idea Index Page
 app.get('/ideas', (req, res) => {
     Idea.find({})
-    .sort({date: 'desc'})
-    .then(ideas => {
-        res.render('ideas/index', {
-            ideas
+        .sort({date: 'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas
+            });
         });
-    })
-})
+});
 
 // Add Idea Form route
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
-})
+});
+
+// Edit Idea Form route
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea
+            });
+        });
+});
 
 // Submit Idea Route
 app.post('/ideas', (req, res) => {
@@ -82,12 +98,17 @@ app.post('/ideas', (req, res) => {
             details: req.body.details,
         };
         new Idea(newUser)
-        .save()
-        .then(idea => {
-            res.redirect('/ideas');
-        })
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            });
     }
-})
+});
+
+// Submit Idea Edit Route
+app.put('/ideas/:id', (req, res) => {
+    res.send('PUT');
+});
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
